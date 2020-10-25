2dc145bedb670b5fb8f0f43b156921a613b7e2d2
Vladimir <alfa.voland@gmail.com>
2020-10-25 14:58:39 +0300

    parsing next pages while they exist

--- main.py
+++ main.py
@@ -3,43 +3,46 @@ from parsing_functions import *
 
 
 def main(main_url):
-    print('Parse first main page...')
+    n = 1
+    while True:
+        print('Parse', n, 'main page...')
 
-    first_page_response = html_response(main_url, WEB_HEADERS)
+        main_page_response = html_response(main_url, WEB_HEADERS)
 
-    cars_brand_year_power = parse_brand_year_power(first_page_response)
-    cars_prices = parse_prices(first_page_response)
-    cars_cities = parse_cities(first_page_response)
-    cars_urls = parse_urls(first_page_response)
+        cars_brand_year_power = parse_brand_year_power(main_page_response)
+        cars_prices = parse_prices(main_page_response)
+        cars_cities = parse_cities(main_page_response)
+        cars_urls = parse_urls(main_page_response)
 
-    brand_year_power_prices = []
-    for data, price in zip(cars_brand_year_power, cars_prices):
-        data['price'] = price
-        brand_year_power_prices.append(data)
+        brand_year_power_prices = []
+        for data, price in zip(cars_brand_year_power, cars_prices):
+            data['price'] = price
+            brand_year_power_prices.append(data)
 
-    brand_year_power_prices_cities = []
-    for data, city in zip(brand_year_power_prices, cars_cities):
-        data['city'] = city
-        brand_year_power_prices_cities.append(data)
+        brand_year_power_prices_cities = []
+        for data, city in zip(brand_year_power_prices, cars_cities):
+            data['city'] = city
+            brand_year_power_prices_cities.append(data)
 
-    brand_year_power_prices_cities_urls = []
-    for data, url in zip(brand_year_power_prices, cars_urls):
-        data['url'] = url
-        brand_year_power_prices_cities_urls.append(data)
+        brand_year_power_prices_cities_urls = []
+        for data, url in zip(brand_year_power_prices, cars_urls):
+            data['url'] = url
+            brand_year_power_prices_cities_urls.append(data)
 
-    for car in brand_year_power_prices_cities_urls:
-        print('Parse', car['brand_model'], car['year'], 'url:', car['url'])
-        first_car_response = html_response(car['url'], WEB_HEADERS)
-        car_description_odometer = parse_description_odometer(first_car_response)
-        car.update(car_description_odometer)
-        sleep(1)
+        for car_data in brand_year_power_prices_cities_urls:
+            print('Parse', car_data['brand_model'], car_data['year'], 'url:', car_data['url'])
+            car_response = html_response(car_data['url'], WEB_HEADERS)
+            car_description_odometer = parse_description_odometer(car_response)
+            car_data.update(car_description_odometer)
+            sleep(1)
 
-    file_name = file_name_path_data_name_d_m_h()
-    write_data_in_file(brand_year_power_prices_cities_urls, file_name)
-    json_to_xlsx()
+        file_name = file_name_path_data_name_d_m_h()
+        write_data_in_file(brand_year_power_prices_cities_urls, file_name)
 
-    next_url = parse_url_next_page(first_page_response)
-    print(next_url)
+        next_main_url = parse_url_next_page(main_page_response)
+        if not next_main_url:
+            break
+        n += 1
 
 
 if __name__ == '__main__':
